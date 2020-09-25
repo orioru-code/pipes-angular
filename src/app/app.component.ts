1e2d294bc460d25db10a05ae255af8a907d5926d
oriol <omart1881@gmail.com>
2020-09-25 14:32:02 +0200

    Pipes iniciado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = 'Capitan América';
+  array: number[] = [1, 2, 3, 4, 5];
 }
