2f1796479cd56528393550705fd4dd88fe19650f
oriol <omart1881@gmail.com>
2020-09-25 15:23:01 +0200

    tabla de pipes acabada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre: string = 'Capitan América';
+  nombre2: string = 'oRioL MarTiNEz';
   array: number[] = [1, 2, 3, 4, 5];
+  PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  heroe = {
+    nombre: 'Logan',
+    clave: 'wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Charles Xavier Mansion',
+      numero: '100',
+    },
+  };
+
+  valorPromise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Llega la promesa');
+    }, 4500);
+  });
+
+  fecha: Date = new Date();
 }
